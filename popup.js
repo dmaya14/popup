28bf8fe6063068faf6c2222d69de916ff6fb3a1b
Daniel Maya <75802476+dmaya14@users.noreply.github.com>
2024-12-25 10:19:06 -0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 // URL del archivo JSON
-const popupURL = "main/popup.json"; // Cambia esta URL a donde tengas tu JSON
+const popupURL = "popup.json"; // Cambia esta URL a donde tengas tu JSON
 
 // Función para cargar y mostrar el pop-up
 async function loadPopup() {
